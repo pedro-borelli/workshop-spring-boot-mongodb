b0637b03e11c4b7ab086bc47734d731065026834
pedro-borelli <borellipedroluiz@gmail.com>
2023-03-22 18:10:37 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/pedroborelli/workshopmongo/domain/User.java
+++ src/main/java/com/pedroborelli/workshopmongo/domain/User.java
@@ -1,12 +1,17 @@
 package com.pedroborelli.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document(collection = "user")
 public class User implements Serializable {
 
     //atributes
 
+    @Id
     private String id;
     private String name;
     private String email;
---
+++ src/main/java/com/pedroborelli/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.pedroborelli.workshopmongo.services;
+
+import com.pedroborelli.workshopmongo.domain.User;
+import com.pedroborelli.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+
+}
