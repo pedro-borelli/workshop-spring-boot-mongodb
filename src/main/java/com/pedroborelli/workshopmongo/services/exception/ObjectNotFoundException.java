3f80968f04903e27281e07e3012cda2daf7199ce
pedro-borelli <borellipedroluiz@gmail.com>
2023-03-23 15:26:11 -0300

    [200~Obtendo um usuário por id

---
+++ src/main/java/com/pedroborelli/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.pedroborelli.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
