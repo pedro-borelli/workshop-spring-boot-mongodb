8e09945f460110fbe5ca0121785c2c94f0ae8cb4
pedro-borelli <borellipedroluiz@gmail.com>
2023-03-23 15:41:42 -0300

    Inserção de usuário com POST

--- src/main/java/com/pedroborelli/workshopmongo/services/UserService.java
+++ src/main/java/com/pedroborelli/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.pedroborelli.workshopmongo.services;
 
 import com.pedroborelli.workshopmongo.domain.User;
+import com.pedroborelli.workshopmongo.dto.UserDTO;
 import com.pedroborelli.workshopmongo.repository.UserRepository;
 import com.pedroborelli.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,15 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public  User insert(User obj) {
+        return repo.insert(obj);
+
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 
 }
